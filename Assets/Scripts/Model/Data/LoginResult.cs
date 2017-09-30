54e24a9c436e1d45784ec067c475fe054b37347f
Chris <sjhshy@gmail.com>
2017-09-30 16:26:32 +0900

    first commit

---
+++ Assets/Scripts/Model/Data/LoginResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class LoginResult {
+
+	public User Data;
+	public int ResultCode;
+	public string Message;
+
+}
---
+++ Assets/Scripts/Model/Data/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class User {
+
+	public string FacebookID;
+	public string FacebookName;
+	public string FacebookPhotoURL;
+
+}
